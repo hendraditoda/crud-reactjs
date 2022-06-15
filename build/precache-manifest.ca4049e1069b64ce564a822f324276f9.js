31d839739d05d63da11f3986f2a8068edf1de6c3
Hendradito Dwi A <hendradito21@gmail.com>
2022-06-15 22:19:43 +0700

    init

---
+++ build/precache-manifest.ca4049e1069b64ce564a822f324276f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce7442329e860c919b248cb9df52816",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b41cc131d43626c5e933",
+    "url": "/static/css/2.2791e197.chunk.css"
+  },
+  {
+    "revision": "879ab0562957324dc89d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b41cc131d43626c5e933",
+    "url": "/static/js/2.021d8e0a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.021d8e0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "879ab0562957324dc89d",
+    "url": "/static/js/main.6f8a25d4.chunk.js"
+  },
+  {
+    "revision": "4f02bfba67e179acac11",
+    "url": "/static/js/runtime-main.5e40ee0a.js"
+  }
+]);
